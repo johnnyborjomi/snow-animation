b97120684c0596a9b8b5a65ba19ebfbc656d50c8
nikita-lysenko <nikita.lysenko@spryker.com>
2019-04-14 01:37:05 +0300

    random flakes add

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 const requestAnimationFrame = window.requestAnimationFrame;
 
+const flakesCount = 100;
+
 function setCanvasDimensions() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -10,11 +12,15 @@ function setCanvasDimensions() {
 setCanvasDimensions();
 // window.addEventListener('resize', setCanvasDimensions);
 
+function getRandomNum(from, to) {
+    return Math.random()
+}
+
 class Flake {
     constructor() {
-        this.radius = 10;
-        this.x = 50;
-        this.y = 50;
+        this.radius = Math.floor((Math.random() * (10 - 5)) + 5);
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
     };
 
     draw() {
@@ -25,14 +31,17 @@ class Flake {
     }
 }
 
-var flake = new Flake();
+var flakes = [];
+for (let i = 0; i <= flakesCount; i++) {
+    flakes.push(new Flake());
+}
 
 function snow() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    flake.draw();
-    flake.y++;
-
-
+    flakes.forEach((flake) => {
+        flake.draw();
+        flake.y++;
+    })
     requestAnimationFrame(snow);
 }
 

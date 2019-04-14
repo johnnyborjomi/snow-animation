3384b5481ca7869f2191d199ebdceae8e418b3db
nikita-lysenko <nikita.lysenko@spryker.com>
2019-04-14 03:09:12 +0300

    second canvas added

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas-snow-fall');
+const canvasHeap = document.getElementById('canvas-snow-heap');
+const ctx = canvas.getContext('2d');
+const ctxHeap = canvasHeap.getContext('2d');
 const requestAnimationFrame = window.requestAnimationFrame;
+
+(function setCanvasDimensions() {
+    canvas.width = canvasHeap.width = window.innerWidth;
+    canvas.height = canvasHeap.height = window.innerHeight;
+})();
 
 const flakesCount = 100;
 
-function setCanvasDimensions() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+const macImg = new Image();
+macImg.src = 'mac.png';
+macImg.width = 305;
+macImg.height = 244;
 
-setCanvasDimensions();
+macImg.addEventListener('load', () => {
+    ctxHeap.drawImage(macImg, (canvasHeap.width - macImg.width) / 2, canvasHeap.height - macImg.height, macImg.width, macImg.height);
+});
+
 // window.addEventListener('resize', setCanvasDimensions);
 
-function getRandomNum(from, to) {
-    return Math.random()
+function getRandomInteger(from, to) {
+    return Math.round(Math.random() * (to - from) + from);
 }
 
 class Flake {
     constructor() {
-        this.radius = Math.floor((Math.random() * (10 - 5)) + 5);
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.radius = getRandomInteger(3, 8);
+        this.x = Math.round(Math.random() * canvas.width);
+        this.y = Math.round(Math.random() * canvas.height);
+        this.opacity = ((Math.random() * 0.5) + 0.5).toFixed(1);
+        this.speed = getRandomInteger(1, 5);
     };
 
     draw() {
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = `rgba(255,255,255,${this.opacity})`;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
+    }
+
+    checkFlakeState() {
+
+        if (this.y >= canvas.height) {
+            if (Number(this.opacity) > 0.7) {
+                ctxHeap.fillStyle = 'white';
+                ctxHeap.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+                ctxHeap.fill();
+            }
+            this.y = 0;
+            this.x = Math.round(Math.random() * canvas.width);
+        }
+
     }
 }
 
@@ -39,8 +65,9 @@ for (let i = 0; i <= flakesCount; i++) {
 function snow() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     flakes.forEach((flake) => {
+        flake.checkFlakeState();
         flake.draw();
-        flake.y++;
+        flake.y += flake.speed;
     })
     requestAnimationFrame(snow);
 }

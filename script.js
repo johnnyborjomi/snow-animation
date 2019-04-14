c82053d1529b35949a6b09b59774dba65ac6736c
nikita-lysenko <nikita.lysenko@spryker.com>
2019-04-14 22:26:22 +0300

    glide add

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const canvasHeap = document.getElementById('canvas-snow-heap');
 const ctx = canvas.getContext('2d');
 const ctxHeap = canvasHeap.getContext('2d');
 const requestAnimationFrame = window.requestAnimationFrame;
+const config = {
+    glide: 2,
+    softness: 0,
+    speed: 1,
+    flakeSizeMin: 1,
+    flakeSizeMax: 8
+};
 
 (function setCanvasDimensions() {
     canvas.width = canvasHeap.width = window.innerWidth;
@@ -22,17 +29,17 @@ macImg.addEventListener('load', () => {
 
 // window.addEventListener('resize', setCanvasDimensions);
 
+
+
 function getRandomInteger(from, to) {
     return Math.round(Math.random() * (to - from) + from);
 }
 
 class Flake {
     constructor() {
-        this.radius = getRandomInteger(3, 8);
+        this.resetFlake();
         this.x = Math.round(Math.random() * canvas.width);
         this.y = Math.round(Math.random() * canvas.height);
-        this.opacity = ((Math.random() * 0.5) + 0.5).toFixed(1);
-        this.speed = getRandomInteger(1, 5);
     };
 
     draw() {
@@ -43,18 +50,46 @@ class Flake {
     }
 
     checkFlakeState() {
+        this.checkHeap();
 
         if (this.y >= canvas.height) {
             if (Number(this.opacity) > 0.7) {
-                ctxHeap.fillStyle = 'white';
-                ctxHeap.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-                ctxHeap.fill();
+                this.putToHeap();
             }
-            this.y = 0;
-            this.x = Math.round(Math.random() * canvas.width);
+            this.resetFlake();
         }
-
     }
+
+    resetFlake() {
+        this.radius = getRandomInteger(config.flakeSizeMin, config.flakeSizeMax);
+        this.opacity = ((Math.random() * 0.5) + 0.5).toFixed(1);
+        this.speed = getRandomInteger(1, config.speed);
+        this.y = 0;
+        this.x = Math.round(Math.random() * canvas.width);
+    }
+
+    checkHeap() {
+        if (ctxHeap.getImageData(this.x, this.y - this.radius * config.softness, 1, 1).data.join() != [0, 0, 0, 0].join()) {
+            if (ctxHeap.getImageData(this.x + config.glide, this.y - this.radius * config.softness, 1, 1).data.join() == [0, 0, 0, 0].join()) {
+                this.x += config.glide;
+            }
+            else if (ctxHeap.getImageData(this.x - config.glide, this.y - this.radius * config.softness, 1, 1).data.join() == [0, 0, 0, 0].join()) {
+                this.x -= config.glide;
+            }
+            else {
+                this.putToHeap();
+                this.resetFlake();
+            }
+        }
+    }
+
+    putToHeap() {
+        ctxHeap.fillStyle = 'white';
+        ctxHeap.beginPath();
+        ctxHeap.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctxHeap.fill();
+    }
+
 }
 
 var flakes = [];
@@ -64,7 +99,7 @@ for (let i = 0; i <= flakesCount; i++) {
 
 function snow() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    flakes.forEach((flake) => {
+    flakes.map((flake) => {
         flake.checkFlakeState();
         flake.draw();
         flake.y += flake.speed;
@@ -73,3 +108,14 @@ function snow() {
 }
 
 snow();
+
+
+
+var gui = new dat.GUI();
+
+gui.add(config, 'glide', 0, 5).step(1);
+gui.add(config, 'softness', 0, 1).step(0.1);
+gui.add(config, 'speed', 1, 10).step(1);
+gui.add(config, 'flakeSizeMin', 1, 10).step(1);
+gui.add(config, 'flakeSizeMax', 1, 10).step(1);
+
